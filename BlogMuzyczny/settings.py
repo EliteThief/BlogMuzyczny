e9ddc8081f930b7ef8da0231dc859e262b216183
emikoNauroka <emillia42nawrocka@gmail.com>
2020-12-02 17:44:15 +0100

    Dotychczasowa BD
    Potrzebny redesign.

--- BlogMuzyczny/settings.py
+++ BlogMuzyczny/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'musicblog.apps.MusicblogConfig',
+    'content.apps.ContentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- content/models.py
+++ content/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+
+class Channel(models.Model):
+    channel_name = models.CharField(max_length=50)
+    date_of_creation = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class VideoInfo(models.Model):
+    artist_name = models.CharField(max_length=70)
+    album_name = models.CharField(max_length=70)
+    year_of_prod = models.DateField(default=timezone.now)
+    description = models.TextField(max_length=5000)
+
+
+class Video(models.Model):
+    views = models.IntegerField()
+    upvotes = models.IntegerField()
+    downvotes = models.IntegerField()
+    video_info = models.ForeignKey(VideoInfo, on_delete=models.CASCADE)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    file = models.FileField()
+
+
+class CommentSection(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    date_of_posting = models.DateTimeField(default=timezone.now)
+    content = models.CharField(max_length=400)
+    upvotes = models.IntegerField()
+    downvotes = models.IntegerField()
+    comment_section = models.ForeignKey(CommentSection, on_delete=models.CASCADE)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
